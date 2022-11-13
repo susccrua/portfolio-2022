1a807b5b426d5389d4a5f9a3ddf126829f876d45
susccrua <susccrua@gmail.com>
2022-11-13 14:01:32 -0600

    Add state to Popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import Spline from '@splinetool/react-spline';
 import Popup from './components/Popup.jsx'
+import Contact from './components/Contact.jsx'
+import { useState } from 'react';
 
 export default function App() {
+  const [popupOpen, setPopupOpen] = useState(false);
+
+  const onWheel = (e) => {
+    setPopupOpen(true);
+  }
+
+  setTimeout(() => { setPopupOpen(false) }, 5000)
   return (
     <div className="relative w-full h-screen">
-      <Popup />
-      <Spline scene="https://prod.spline.design/qoJQKBNdU85xEJU8/scene.splinecode" />
+      <Popup isOpen={popupOpen} />
+      <Contact />
+      <Spline scene="https://prod.spline.design/qoJQKBNdU85xEJU8/scene.splinecode"
+        onWheel={onWheel}
+      // onCollision={onCollision}
+      />
     </div>
   );
 }
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,8 +1,12 @@
 export default function Popup(props) {
-    const { hidden } = props;
+    const { isOpen } = props;
+    const hidden = isOpen ? 'block' : 'hidden';
     return (
-        <div className="absolute mt-60 ml-40 w-80 h-40 rounded-lg bg-white opacity-60 p-4">
-            <a className="absolute h-10 text-lg text-gray-400 font-bold">Susana's future portfolio</a>
+        <div className={`absolute mt-40 ml-20 w-60 h-40 rounded-lg bg-white opacity-60 p-4 ${hidden}`}>
+            <section className="h-10 text-lg text-gray-400 font-bold">
+                <p>{"Press keys"} <br />{" A & D or < & > "} <br />{"to move!"}</p>
+
+            </section>
         </div>
         // <div></div>
     )
