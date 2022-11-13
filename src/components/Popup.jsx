55bbcf12ac7f06494d3a21b45564687e4bf4a4c9
susccrua <susccrua@gmail.com>
2022-11-12 18:37:01 -0600

    Added scene and styles

---
+++ src/components/Popup.jsx
@@ -0,0 +1,10 @@
+export default function Popup(props) {
+    const { hidden } = props;
+    return (
+        <div className="absolute mt-60 ml-40 w-80 h-40 rounded-lg bg-white opacity-60 p-4">
+            <a className="absolute h-10 text-lg text-gray-400 font-bold">Susana's future portfolio</a>
+        </div>
+        // <div></div>
+    )
+
+}
